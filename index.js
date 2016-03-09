f8b7654b3339dd4844d43475245145130225a402
Julian <julian@neat.mx>
2016-03-09 11:11:10 -0600

    Reformats code.
    Applies our own coding standards ( spaces between parenthesis,
    spaces instead of tabs, etc. ).

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-var postcss = require('postcss');
+var postcss = require( 'postcss' );
 
-module.exports = postcss.plugin('postcss-remify', function (opts) {
-    opts = opts || { base : 16, fallback : false };
+module.exports = postcss.plugin( 'postcss-remify', function( opts ) {
+  opts = opts || { base : 16, fallback : false };
 
-    var convertToRem = function( expression ) {
-      var base = opts.base;
-      var exp = /rem\(([^)]+)\)/;
+  var convertToRem = function( expression ) {
+    var base = opts.base;
+    var exp  = /rem\(([^)]+)\)/;
 
-      var match = exp.exec( expression );
-      var fallback = match[1] + 'px';
-      var value = match[1] / base + 'rem';
+    var match    = exp.exec( expression );
+    var fallback = match[ 1 ] + 'px';
+    var value    = match[ 1 ] / base + 'rem';
 
-      return { rem : value, fallback : fallback };
-    };
+    return { rem : value, fallback : fallback };
+  };
 
-    return function (css) {
-      css.walkDecls( function (decl) {
-        var exp = /rem\(([^)]+)\)/g;
-        var matches = decl.value.match( exp );
-        var fallback = decl.value;
+  return function( css ) {
+    css.walkDecls( function( decl ) {
+      var exp      = /rem\(([^)]+)\)/g;
+      var matches  = decl.value.match( exp );
+      var fallback = decl.value;
 
-        if (! matches) {
-          return;
+      if ( ! matches ) {
+        return;
+      }
+
+      matches.forEach( function( match, index ) {
+        var values = convertToRem( match );
+
+        if ( opts.fallback ) {
+          fallback = fallback.replace( match, values.fallback );
         }
 
-        matches.forEach( function( match, index ) {
-          var values = convertToRem( match );
+        decl.value = decl.value.replace( match, values.rem );
+      } );
 
-          if (opts.fallback) {
-            fallback = fallback.replace( match, values.fallback );
-          }
-
-          decl.value = decl.value.replace( match, values.rem );
-        });
-
-        if (opts.fallback) {
-          decl.cloneBefore({
-            prop  : decl.prop,
-            value : fallback
-          });
-        }
-      });
-    };
-});
+      if ( opts.fallback ) {
+        decl.cloneBefore( {
+          prop  : decl.prop,
+          value : fallback
+        } );
+      }
+    } );
+  };
+} );

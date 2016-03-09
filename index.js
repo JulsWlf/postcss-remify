334c49aa84336482f9d5af411b8e986b28cd0358
Julian <julian@neat.mx>
2016-03-09 11:10:04 -0600

    Allows for multiple declarations.
    Improves expression matching and rem convertion, allowing
    for multiple expressions on the same declaration without
    breaking it.

--- index.js
+++ index.js
@@ -3,35 +3,43 @@ var postcss = require('postcss');
 module.exports = postcss.plugin('postcss-remify', function (opts) {
     opts = opts || { base : 16, fallback : false };
 
-    if (typeof opts.base === 'undefined') {
-      opts.base = 16;
-    }
+    var convertToRem = function( expression ) {
+      var base = opts.base;
+      var exp = /rem\(([^)]+)\)/;
 
-    if (typeof opts.fallback === 'undefined') {
-      opts.fallback = false;
-    }
+      var match = exp.exec( expression );
+      var fallback = match[1] + 'px';
+      var value = match[1] / base + 'rem';
+
+      return { rem : value, fallback : fallback };
+    };
 
     return function (css) {
-        css.walkDecls( function (decl) {
-            var base = opts.base;
+      css.walkDecls( function (decl) {
+        var exp = /rem\(([^)]+)\)/g;
+        var matches = decl.value.match( exp );
+        var fallback = decl.value;
 
-            if (!decl.value || decl.value.indexOf('rem(') === -1) {
-                return;
-            }
+        if (! matches) {
+          return;
+        }
 
-            var index = decl.value.indexOf('(');
-            var last  = decl.value.indexOf(')');
-            var value = decl.value.slice(++index, last);
-            value     = value.match(/\d+/)[0];
+        matches.forEach( function( match, index ) {
+          var values = convertToRem( match );
 
-            if (opts.fallback) {
-              decl.cloneBefore({
-                prop  : 'font-size',
-                value : value + 'px'
-              });
-            }
+          if (opts.fallback) {
+            fallback = fallback.replace( match, values.fallback );
+          }
 
-            decl.value = value / base + 'rem';
+          decl.value = decl.value.replace( match, values.rem );
         });
+
+        if (opts.fallback) {
+          decl.cloneBefore({
+            prop  : decl.prop,
+            value : fallback
+          });
+        }
+      });
     };
 });
